fb9e7ea8fb5566c35c59a638d1726521bebfad0b
CauaS <37887533+CauaS@users.noreply.github.com>
2019-03-08 06:47:17 -0300

    Add files via upload

--- src/Map/index.js
+++ src/Map/index.js
@@ -1,13 +1,16 @@
 import React,  { Component } from 'react';
 import { View } from 'react-native';
 import MapView from 'react-native-maps';
+import Search from '../Search/index';
+import Directions from '../Directions/index';
 
 export default class Map extends Component {
     state = {
-        region: null
+        region: null,
+        destination: null
     }
-    async componentDidMount() {
-        navigator.geolocation.getCurrentPosition(
+   async componentDidMount() {
+        navigator.geolocation.getCurrentPosition(            
             ({ coords: { latitude, longitude }}) => {
                 this.setState({ region: {
                     latitude,
@@ -16,24 +19,55 @@ export default class Map extends Component {
                     longitudeDelta: 0.0134,
                 }})
             }, //sucesso,
-            () => {}, //erro
+            (erro) => {console.log(erro)}, //erro
             {
                 timeout: 2000, //2 sec pra cair no erro
                 enableHighAccuracy: true, // utilizar gps
-                maximumAge: 1000 //cash de localização de no maximo 1 seg, intervalo
+                maximumAge: 1000 //cache de localização de no maximo 1 seg, intervalo
             }
         );
     }
+    handleLocationSelected = (data, { geometry }) => {
+        const { location: { lat: latitude, lng: longitude }} = geometry 
+
+        this.setState({
+            destination: {
+                latitude,
+                longitude,
+                title: data.structured_formatting.main_text,
+            }
+        })
+    }
+    
     render() {
-        const { region } = this.state;
+        const { region, destination } = this.state;
         return (
-            <View style={{ flex: 1}}>
+            <View style={{ flex: 1}}>            
                 <MapView 
                     style={{ flex: 1}}
-                    region={region}
+                    region={{ 
+                        latitude: -29.5820061, 
+                        longitude: -51.0834943,
+                        latitudeDelta: 0.0143,
+                        longitudeDelta: 0.0134
+                    }}
                     showsUserLocation
                     loadingEnabled
-                />
+                >
+                { destination && (
+                    <Directions 
+                        origin={{
+                            latitude: -29.5820061, 
+                            longitude: -51.0834943,
+                            atitudeDelta: 0.0143,
+                            longitudeDelta: 0.0134
+                        }} 
+                        destination={destination}
+                        onReady={() => {}}
+                    />
+                )}
+                </MapView> 
+                <Search onLocationSelected={ this.handleLocationSelected}/>
             </View>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import MapView from 'react-native-maps';
 import Map from './Map/index';
 
 const MainApp = () => <Map />;
