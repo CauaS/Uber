bb354a31c9226a4f272a1806b6f7462fad1fe232
Caligiuri Cauã <37887533+CauaS@users.noreply.github.com>
2021-03-10 17:11:05 -0300

    Update index.js

--- src/Directions/index.js
+++ src/Directions/index.js
@@ -6,7 +6,7 @@ const Directions = ({ destination, origin, onReady }) => (
     destination={destination}
     origin={origin}
     onReady={onReady}
-    apikey="AIzaSyDD5BM8MmH_Fk7qVOR8gvYNlGvjHp45Xmo"
+    apikey="..."
     strokeWidth={3}
     strokeColor="#222"
   />
